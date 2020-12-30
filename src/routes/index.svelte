2f6a38a7304c326e3a22171edcc49485ac633341
nusendra <admin@nusendra.com>
2020-12-31 01:11:29 +0700

    feat: added animate flip

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,13 +4,13 @@
 
 <script>
   import {flip} from 'svelte/animate';
-  import Card from '../components/Card.svelte';
   import { columns } from '../stores/index.js';
 
   let _columns;
   let newRowDescription;
   let showRowInput = false;
   let activeColumnIndex;
+  let hoveringColumn;
 
   columns.subscribe(value => {
     _columns = value;
@@ -31,11 +31,41 @@
       showRowInput = false;
 		}
   }
+
+  const dragStart = (event, columnIndex, itemIndex) => {
+    console.log(event)
+		const data = {columnIndex, itemIndex};
+   	event.dataTransfer.setData('text/plain', JSON.stringify(data));
+	}
+
+  function drop(event, index) {
+    console.log(event)
+		event.preventDefault();
+    const json = event.dataTransfer.getData("text/plain");
+		const data = JSON.parse(json);
+
+      console.log(data)
+
+		const [item] = _columns[data.columnIndex].items.splice(data.itemIndex, 1);
+
+		_columns[index].items.unshift(item);
+		_columns = _columns;
+
+		hoveringColumn = null;
+	}
 </script>
 
+<pre>{ hoveringColumn }</pre>
 <div class="columns" style="margin: 10px;">
-  {#each _columns as { name, items }, index}
-    <div class="column">
+  {#each _columns as { name, items }, index (index)}
+    <div
+      animate:flip
+      class="column"
+      on:dragenter={() => hoveringColumn = name}
+      on:dragleave={() => hoveringColumn = null}
+      on:drop={event => drop(event, index)}
+      ondragover="return false"
+    >
       {#if name !== ''}
         <div class="level">
           <div class="level-left">
@@ -54,10 +84,23 @@
             on:keyup|preventDefault={submit}
             style="margin-bottom: 10px;"
           >
-         {/if}
-       {/if}
-      {#each items as item}
-        <Card description={item}/>
+        {/if}
+      {/if}
+      {#each items as item, itemIndex (item)}
+        <div animate:flip>
+          <div
+            class="card"
+            style="margin-bottom: 10px;"
+            draggable={true}
+		  		  on:dragstart={event => dragStart(event, index, itemIndex)}
+          >
+            <div class="card-content">
+              <p class="subtitle">
+                {item}
+              </p>
+            </div>
+          </div>
+        </div>
       {/each}
     </div>
   {/each}
